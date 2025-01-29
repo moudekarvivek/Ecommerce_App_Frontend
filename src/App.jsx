4df4ece519d28787c6953ae5c10d8e7041f52a42
moudekarvivek <moudekarvivek@gmail.com>
2025-01-29 20:49:30 +0530

    Setting up project and essential dependencies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { FaBeer } from 'react-icons/fa'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-800 text-white text-2xl font-bold">
+      WELCOME <FaBeer />
+    </div>
+  )
+}
+
+export default App
