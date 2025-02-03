d0c1a5365e007373a78aeb00db06efb623577569
moudekarvivek <moudekarvivek@gmail.com>
2025-02-03 17:31:45 +0530

    Managing Loaders and Errors Scenarios

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,6 +2,7 @@ import api from "../../api/api";
 
 export const fetchProducts = () => async (dispatch) => {
     try {
+        dispatch({ type: "IS_FETCHING" });
         const { data } = await api.get(`/public/products`);
         console.log("API Response:", data);
         dispatch({
@@ -12,8 +13,13 @@ export const fetchProducts = () => async (dispatch) => {
             totalElements: data.totalElements,
             totalPages: data.totalPages,
             lastPage: data.lastPage,
-        })
+        });
+        dispatch({ type: "IS_SUCCESS" });
     } catch (error) {
-        console.log(error); 
+        console.log(error);
+        dispatch({ 
+            type: "IS_ERROR",
+            payload: error?.response?.data?.message || "Failed to fetch products", //Optional Chaining to know response exist in error
+         }); 
     }
 };
--- src/store/reducers/store.js
+++ src/store/reducers/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./ProductReducer";
+import { errorReducer } from "./errorReducer";
 
 
 export const store = configureStore({ 
     reducer: {
         products: productReducer,
+        errors: errorReducer,
     },
     preloadedState: {},
 });
