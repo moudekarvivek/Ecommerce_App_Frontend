557ae6e1c8423922f35efa6e4568cf706583a4a9
moudekarvivek <moudekarvivek@gmail.com>
2025-02-04 13:26:08 +0530

    Adding Life to Product Filters

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,6 +5,10 @@ import { useEffect } from "react";
 import { fetchProducts } from "../store/actions";
 import Filter from "./Filter";
 
+// http://localhost:xxxx?keyword=test&sortby=desc
+
+// 1. Make sure url is updated with filter values
+//  2. Use this filter values  for getting data from backend
 const Products = () => {
     const { isLoading, errorMessage } = useSelector(
         (state) => state.errors
