1c9c714dd34653e971551be904e4077aca1d2c66
moudekarvivek <moudekarvivek@gmail.com>
2025-02-05 12:30:08 +0530

    Making Categories Dynamic

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -23,3 +23,27 @@ export const fetchProducts = (queryString) => async (dispatch) => {
          }); 
     }
 };
+
+export const fetchCategories = (queryString) => async (dispatch) => {
+    try {
+        dispatch({ type: "CATEGORY_LOADER" });
+        const { data } = await api.get(`/public/categories`);
+        console.log("API Response:", data);
+        dispatch({
+            type: "FETCH_CATEGORIES",
+            payload: data.content,
+            pageNumber: data.pageNumber,
+            pageSize: data.pageSize,
+            totalElements: data.totalElements,
+            totalPages: data.totalPages,
+            lastPage: data.lastPage,
+        });
+        dispatch({ type: "IS_ERROR" });
+    } catch (error) {
+        console.log(error);
+        dispatch({ 
+            type: "IS_ERROR",
+            payload: error?.response?.data?.message || "Failed to fetch category", //Optional Chaining to know response exist in error
+         }); 
+    }
+};
--- src/store/reducers/errorReducer.js
+++ src/store/reducers/errorReducer.js
@@ -1,6 +1,8 @@
 const initialState = {
     isLoading: false,
     errorMessage: null,
+    categoryLoader: false,
+    categoryError: null,
 };
 
 export const errorReducer = (state = initialState, action) => {
@@ -23,6 +25,18 @@ export const errorReducer = (state = initialState, action) => {
                 isLoading: false,
                 errorMessage: action.payload,
             };
+        case "CATEGORY_SUCCESS":
+                return {
+                    ...state,
+                    categoryLoader: false,
+                    categoryError: null,
+                };
+        case "CATEGORY_LOADER":
+                return {
+                    ...state,
+                    categoryLoader: true,
+                    errorMessage: null,
+                };
         default:
             return state;
     }   
