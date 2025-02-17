d09a09bf1d5c7aa94a75d7f1128858480ff90c18
moudekarvivek <moudekarvivek@gmail.com>
2025-02-17 16:21:45 +0530

    Setting up Banner on Home Page created using Swiper React Library

---
+++ src/utils/constant.js
@@ -0,0 +1,8 @@
+import bannerImageOne from "../assets/sliders/sofa.png";
+import bannerImageTwo from "../assets/sliders/Television.png";
+import bannerImageThree from "../assets/sliders/cloth.png";
+export {
+    bannerImageOne,
+    bannerImageTwo,
+    bannerImageThree
+}
