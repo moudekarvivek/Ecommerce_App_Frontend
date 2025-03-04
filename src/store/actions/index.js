608be3357851fe7bf3f0621a14668a9375c57292
moudekarvivek <moudekarvivek@gmail.com>
2025-03-04 10:49:13 +0530

    Allowing Users to Remove Items From Cart

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -106,3 +106,9 @@ export const decreaseCartQuantity =
         });
         localStorage.setItem("cartItems", JSON.stringify(getState().carts.cart));
     }
+
+export const removeFromCart = (data, toast) => (dispatch, getState) => {
+    dispatch({type: "REMOVE_CART", payload: data });
+    toast.success(`${data.productName} removed from the cart`);
+    localStorage.setItem("cartItems", JSON.stringify(getState().carts.cart));
+}
