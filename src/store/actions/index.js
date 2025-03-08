69d8d4a2f1cbf89f1dd1a2f91ebd36156a583b69
moudekarvivek <moudekarvivek@gmail.com>
2025-03-08 16:00:51 +0530

    User Registration: Building the Flow

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -113,15 +113,32 @@ export const removeFromCart = (data, toast) => (dispatch, getState) => {
     localStorage.setItem("cartItems", JSON.stringify(getState().carts.cart));
 }
 
-export const authenticateSignInUser 
+export const registerNewUser 
     = (sendData, toast, reset, navigate, setLoader) => async (dispatch) => {
         try {
             setLoader(true);
-            const { data } = await api.post("/auth/signin", sendData);
+            const { data } = await api.post("/auth/signup", sendData);
+            reset()
+            toast.success(data?.message || "User Registered Successfully");
+            navigate("/login");
+        } catch (error) {
+            console.log(error);
+            toast.error(error?.response?.data?.message || error?.response?.data?.password || "Internal Server Error");
+        } finally {
+            setLoader(false);
+        }
+}
+
+
+export const authenticateSignUpUser 
+    = (sendData, toast, reset, navigate, setLoader) => async (dispatch) => {
+        try {
+            setLoader(true);
+            const { data } = await api.post("/auth/signup", sendData);
             dispatch({ type: "LOGIN_USER", payload: data });
             localStorage.setItem("auth", JSON.stringify(data));
             reset()
-            toast.success("Login Success");
+            toast.success("Register Success");
             navigate("/");
         } catch (error) {
             console.log(error);
