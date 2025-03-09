2fdeadbc9cec23a11249ae2e3f208b00489ee4dc
moudekarvivek <moudekarvivek@gmail.com>
2025-03-09 13:11:20 +0530

    Address Selection:Allowing users to Add Address

--- src/components/checkout/AddressInfo.jsx
+++ src/components/checkout/AddressInfo.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
  import Skeleton from '../shared/Skeleton';
  import { FaAddressBook } from 'react-icons/fa';
+import AddressInfoModal from './AddressInfoModal';
+import AddAddressForm from './AddAddressForm';
  
  const AddressInfo = () => {
- 
+    const [openAddressModal, setOpenAddressModal] = useState(false);
+    const [selectedAddress, setSelectedAddress] = useState("");
+    const addNewAddressHandler = () => {
+        setSelectedAddress("");
+        setOpenAddressModal(true);
+     };
      const noAddressExist = true;
      const isLoading = false;
    return (
@@ -17,6 +24,12 @@ import React from 'react'
                  <p className='mb-6 text-slate-800 text-center'>
                      Please add your address to complete purchase
                  </p>
+
+                 <button
+                    onClick={addNewAddressHandler}
+                    className='px-4 py-2 bg-blue-600 text-white font-medium rounded hover:bg-blue-700 transition-all'>
+                    Add Address
+                 </button>
              </div>
          ) : (
              <div className='relative p-6 rounded-lg max-w-md mx-auto'>
@@ -35,6 +48,12 @@ import React from 'react'
              )}
              </div>
          )}
+
+         <AddressInfoModal 
+            open={openAddressModal}
+            setOpen={setOpenAddressModal}>
+                <AddAddressForm />
+         </AddressInfoModal>
      </div>
    )
  }
