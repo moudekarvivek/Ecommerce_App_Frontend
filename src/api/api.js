e5a288ddbc04c52af52f7caf719f2abe93752750
moudekarvivek <moudekarvivek@gmail.com>
2025-03-11 11:34:48 +0530

    Saving Cookies After Authentication in Frontend

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: `${import.meta.env.VITE_BACK_END_URL}/api`,    // URL of the API server in this case our backend 
+    //Include cookies in the request that is working with
+    withCredentials: true,
 });
 
 export default api;
