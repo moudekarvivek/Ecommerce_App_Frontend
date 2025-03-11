3e788804c6944969fb3034ef3461aa81690b8a24
moudekarvivek <moudekarvivek@gmail.com>
2025-03-11 18:14:47 +0530

    Address Selection: Displaying User Addresses

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -1,13 +1,16 @@
 import { Step, StepLabel, Stepper } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 import AddressInfo from './AddressInfo';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getUserAddresses } from '../../store/actions';
 
 const Checkout = () => {
     
     const [activeStep, setActiveStep] = useState(0);
     const dispatch = useDispatch();
+    const { address } = useSelector(
+        (state) => state.auth 
+    )
 
     const steps = [
         "Address",
@@ -31,7 +34,7 @@ const Checkout = () => {
         </Stepper>
 
         <div className='mt-5'>
-            {activeStep === 0 && <AddressInfo />}
+            {activeStep === 0 && <AddressInfo address={address}/>}
         </div>
     </div>
   );
